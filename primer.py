433079a2ab282eb4add0be3fade93938fb522f71
Cristian Guzmán <cristi.guzmanv@duocuc.cl>
2026-04-01 09:48:44 -0300

    Add files via upload

---
+++ primer.py
@@ -0,0 +1,17 @@
+# print("Hola, mundo")
+
+# Nuestras variables
+#v_Enteras = 42 variable entera = Int
+#v_Decimales = 3.14 variable decimales = Float
+#v_Texto = "2121212" 
+#v_Booleano = True
+
+#print("Variables enteras:", v_Enteras, "Variable tipo texto " + v_Texto)
+
+#identifica si es mayor de edad
+edad=int(input("Dime tu edad: "))
+if edad >= 18:
+    print("Eres Mayor ")
+else:
+    print("Eres menor ")    
+ 
